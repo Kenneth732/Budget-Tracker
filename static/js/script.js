c69b27d71989351e7023fb2187879c41e77a9108
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-21 09:35:17 +0300

    FinanceTracker func.. to add entry of finance

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,10 @@ function FinanceTracker() {
     this.currentId = 0;
 }
 
+// add method for entry
+FinanceTracker.prototype.addEntry = function (type, description, value) {
+    const entry = new Finance(type, description, value);
+    entry.id = ++this.currentId;
+    this.entries.push(entry);
+    return entry;
+};
