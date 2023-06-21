44943ff67291172ea247e8ddfec783a0e5b01b99
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-21 09:36:08 +0300

    FinanceTracker func.. method to handle delete

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,13 @@ FinanceTracker.prototype.addEntry = function (type, description, value) {
     this.entries.push(entry);
     return entry;
 };
+
+// add method for deleteentry
+FinanceTracker.prototype.deleteEntry = function (id) {
+    const index = this.entries.findIndex(entry => entry.id === id);
+    if (index !== -1) {
+        this.entries.splice(index, 1);
+        return true;
+    }
+    return false;
+};
